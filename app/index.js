fbe6901aa24ca5bba501d0be151c8cc542d021e1
sakayaparp <sakayaparp.p@thoughtworks.com>
2021-10-14 15:08:37 +0700

    Init project

---
+++ app/index.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV === 'test') {
+  // behavior for the test environment, we'll try to make it work
+
+  if (typeof window !== 'undefined') {
+    require('.//index.85d959.js');
+    const ctx = window['dbg:piral'];
+    const dependencies = (ctx && ctx.pilets && ctx.pilets.getDependencies({})) || {};
+    module.exports = dependencies['app-shell'] || {};
+  } else {
+    console.error('Your test environment does not define "window". Please make sure to provide a proper environment.');
+    module.exports = {};
+  }
+} else {
+  // under "normal" circumstances disallow such an import
+  throw new Error("This file should not be included anywhere. Usually, this means you've imported the Piral instance directly.");
+}
